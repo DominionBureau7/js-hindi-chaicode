dd3ce1ea50485f79541afcc61ab0cdcdf23cd6ef
DominionBureau7 <samink52ns@gmail.com>
2025-12-23 08:36:58 +0000

    dataTypes

---
+++ .github/01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; /* treat all js code as newer version*/
+// alert("hello");
+// alert(3+3); we are using nodejs, not browser
+
+// node .github/01_basics/02_dataTypes.js
+
+let name = "hitesh" /*string*/
+let age = 18 /*number*/
+let isLoggedIn = false
+console.log(typeof "hitesh");
+console.log(typeof age);
